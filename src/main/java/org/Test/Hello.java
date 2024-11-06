ddbe6e8c7db920ac4ab237bc8bcec0843bd7fd66
RaghulRamachandran <ramachandran2898@gmail.com>
2024-11-06 12:11:01 +0000

    Hello

--- src/main/java/org/Test/Hello.java
+++ src/main/java/org/Test/Hello.java
@@ -3,6 +3,7 @@ package org.Test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-    }
+        System.out.println("Hi there");
 
+    }
 }
