fb760bc9d60a4f399d8e71095ffda4c81e93e7b7
Ramachandran Annamalai <86276917+RaghulRamachandran@users.noreply.github.com>
2024-11-12 06:50:35 +0000

    Update HelloWorld.java

--- src/main/java/org/Test/HelloWorld.java
+++ src/main/java/org/Test/HelloWorld.java
@@ -3,6 +3,7 @@ package org.Test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hellowordl!");
+        System.out.println("Hey how are you");
     }
 
 }
